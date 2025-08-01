d287143439b930d2454bdba74db01cefff069dc9
Victor Luhan <92903765+VictorLuhan@users.noreply.github.com>
2025-08-01 03:25:51 -0300

    refactor: update image remote patterns and increase body parser size limit

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,48 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Configurações do Next.js */
   reactStrictMode: true,
+
+  // Permitir imagens externas
   images: {
     remotePatterns: [
+      // API principal
       {
         protocol: 'https',
-        hostname: 'api.sortenaraspada.site',
+        hostname: 'api.raspadinhabr.online',
         port: '',
         pathname: '/uploads/**',
       },
       // Supabase CDN
       {
         protocol: 'https',
-        hostname: '**.supabase.co',
+        hostname: '*.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
-      }
+      },
+      // ImgBB - página raiz (liberar qualquer caminho)
+      {
+        protocol: 'https',
+        hostname: 'ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      // ImgBB - CDN real (liberar qualquer caminho)
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
+  },
+
+  // Se você usa API Routes, aumentar o limite do body (caso uploads grandes)
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb', // Ajuste o tamanho máximo
+    },
   },
 };
 
